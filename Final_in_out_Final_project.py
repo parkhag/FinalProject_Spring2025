769d0e67c7f40163ce2bfb65bb7061fbd58cc0ca
Acerpol <132271136+Acerpol@users.noreply.github.com>
2025-03-14 18:00:13 -0400

    Added Minor GUI integration with Tkinter

--- Final_in_out_Final_project.py
+++ Final_in_out_Final_project.py
@@ -5,6 +5,9 @@ Created on Fri Mar 14 12:11:08 2025
 @author: alexa
 """
 
+import tkinter as tk
+
+root = tk.Tk()
 
 
 class open_file():
@@ -21,23 +24,18 @@ class open_file():
         
     def read(self): #Just Read file
         with open(self.path, 'r') as text_file:
-            print(text_file.read())
-        print("------------------")
+            content = text_file.read()
+            print(content)
+            text_box.delete("1.0", tk.END)  # Clear previous content
+            text_box.insert(tk.END, content)  # Display file content
         
         
-        
-        
-        
-        
+#Unfinished....
 
     def append_content(self, new_line):
         with open(self.path, 'r+') as text_file:
             text_file.write(new_line)
         # Connected to read_write function
-            
-            
-            
-            
             
             
     def read_write(self):   # Enable Read and Write
@@ -49,24 +47,34 @@ class open_file():
         print("---------------")
         # Unfinished # add ammend instead of read
 
+#------------------------
 
-    
-        
-
-
-        
-     
+# User input for file path, etc.
 file_path = "C:/Users/alexa/OneDrive/Documents/Text Files/"
 file_name = str(input('Enter File Name: '))
+my_file = open_file(file_path, file_name)
 
 
-# unused
-path_2 = file_name + "/" + file_name + ".txt"
+# Setup for Popup
+root.title(f"{file_name} File")
+root.geometry("500x400")
+
+# Create a Text widget to display file content
+text_box = tk.Text(root, wrap="word", width=60, height=20)
+text_box.pack(pady=10)
+
+# Create a Button to open the file
+button1 = tk.Button(root, text="Read File", command=my_file.read)
+button1.pack(pady=0)
+button2 = tk.Button(root, text="Create File", command=my_file.create_file)
+button2.pack(pady=0)
 
 
 
 print("\n-----------------------\n")
-my_file = open_file(file_path, file_name)
-my_file.read()
 
-#-----------Creation of files
+
+
+
+#Run popup
+root.mainloop()
